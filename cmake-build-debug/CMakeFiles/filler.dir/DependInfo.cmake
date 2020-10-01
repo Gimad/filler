892593272fddc8df0d5fd177c491597335ac4a9e
Gimad <skullsnake2@gmail.com>
2020-10-01 18:34:13 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/filler.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/Gimad/Desktop/filler1/libft/ft_abs.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_abs.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_atoi.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_atoi.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_bzero.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_bzero.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_memcmp.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_memcmp.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_memdel.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_memdel.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_memset.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_memset.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_putchar.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_putchar.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_putchar_fd.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_putchar_fd.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_putendl.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_putendl.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_putnbr.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_putnbr.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_putstr.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_putstr.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_putstr_fd.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_putstr_fd.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_space.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_space.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_strchr.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_strchr.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_strcmp.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_strcmp.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_strdel.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_strdel.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_strdup.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_strdup.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_strjoin.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_strjoin.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_strlen.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_strlen.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_strncmp.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_strncmp.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_strnew.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_strnew.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_strscroll.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_strscroll.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_strstr.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_strstr.c.o"
+  "/Users/Gimad/Desktop/filler1/libft/ft_strsub.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/libft/ft_strsub.c.o"
+  "/Users/Gimad/Desktop/filler1/srcs/algorithm.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/srcs/algorithm.c.o"
+  "/Users/Gimad/Desktop/filler1/srcs/extra.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/srcs/extra.c.o"
+  "/Users/Gimad/Desktop/filler1/srcs/get_next_line.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/srcs/get_next_line.c.o"
+  "/Users/Gimad/Desktop/filler1/srcs/main.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/srcs/main.c.o"
+  "/Users/Gimad/Desktop/filler1/srcs/read_all.c" "/Users/Gimad/Desktop/filler1/cmake-build-debug/CMakeFiles/filler.dir/srcs/read_all.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/filler.dir/cmake_clean.cmake
@@ -0,0 +1,38 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/filler.dir/libft/ft_abs.c.o"
+  "CMakeFiles/filler.dir/libft/ft_atoi.c.o"
+  "CMakeFiles/filler.dir/libft/ft_bzero.c.o"
+  "CMakeFiles/filler.dir/libft/ft_memcmp.c.o"
+  "CMakeFiles/filler.dir/libft/ft_memdel.c.o"
+  "CMakeFiles/filler.dir/libft/ft_memset.c.o"
+  "CMakeFiles/filler.dir/libft/ft_putchar.c.o"
+  "CMakeFiles/filler.dir/libft/ft_putchar_fd.c.o"
+  "CMakeFiles/filler.dir/libft/ft_putendl.c.o"
+  "CMakeFiles/filler.dir/libft/ft_putnbr.c.o"
+  "CMakeFiles/filler.dir/libft/ft_putstr.c.o"
+  "CMakeFiles/filler.dir/libft/ft_putstr_fd.c.o"
+  "CMakeFiles/filler.dir/libft/ft_space.c.o"
+  "CMakeFiles/filler.dir/libft/ft_strchr.c.o"
+  "CMakeFiles/filler.dir/libft/ft_strcmp.c.o"
+  "CMakeFiles/filler.dir/libft/ft_strdel.c.o"
+  "CMakeFiles/filler.dir/libft/ft_strdup.c.o"
+  "CMakeFiles/filler.dir/libft/ft_strjoin.c.o"
+  "CMakeFiles/filler.dir/libft/ft_strlen.c.o"
+  "CMakeFiles/filler.dir/libft/ft_strncmp.c.o"
+  "CMakeFiles/filler.dir/libft/ft_strnew.c.o"
+  "CMakeFiles/filler.dir/libft/ft_strscroll.c.o"
+  "CMakeFiles/filler.dir/libft/ft_strstr.c.o"
+  "CMakeFiles/filler.dir/libft/ft_strsub.c.o"
+  "CMakeFiles/filler.dir/srcs/algorithm.c.o"
+  "CMakeFiles/filler.dir/srcs/extra.c.o"
+  "CMakeFiles/filler.dir/srcs/get_next_line.c.o"
+  "CMakeFiles/filler.dir/srcs/main.c.o"
+  "CMakeFiles/filler.dir/srcs/read_all.c.o"
+  "filler"
+  "filler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/filler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
